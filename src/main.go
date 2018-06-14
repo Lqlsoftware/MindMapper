b3279671e607186797cab1293c8652d76f78a931
Lqlsoftware <lqlsoft@hotmail.com>
2018-06-14 15:41:33 +0800

    add test commit

--- src/main.go
+++ src/main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+	"github.com/Lqlsoftware/mindmapper/src/model/Tree"
+	"github.com/Lqlsoftware/mindmapper/src/model/git"
 	"github.com/Lqlsoftware/mindmapper/src/orm"
 	"github.com/astaxie/beego"
 )
@@ -14,26 +19,5 @@ func init() {
 }
 
 func main() {
-	//dstCommit, _ := git.LoadCommit(1)
-	//commit := git.Commit{
-	//	Id:			git.GetLastCommitId(),
-	//	Diff:		Tree.MapperDiff{},
-	//	Time:		time.Now().Unix(),
-	//	Title:		"third commit",
-	//	Summary:	"hhhhh",
-	//	Tree:		Tree.MindMapperTree{
-	//		Tree: 	map[string]Tree.TreeNode{
-	//			"0": {"0","-1", "1","root"},
-	//			"1": {"1","0", "1","child1"},
-	//			"2": {"2","0", "2","asdasd"},
-	//			"3": {"3","1", "1","gg"},
-	//			"4": {"4","2", "1","errer"},
-	//		},
-	//		Hash: 	"777",
-	//	},
-	//}
-	//fmt.Println(dstCommit.MergeWith(&commit))
-	//
-	//return
-	//beego.Run()
+	beego.Run()
 }
