233707161394d3c3f07adf833df8d4007f4fe326
Lqlsoftware <lqlsoft@hotmail.com>
2018-06-17 23:33:25 +0800

    add /merge

--- src/handler/mergeHandler.go
+++ src/handler/mergeHandler.go
@@ -1,12 +1,43 @@
 package handler
 
 import (
+	"errors"
+
+	"github.com/Lqlsoftware/mindmapper/src/config"
+	"github.com/Lqlsoftware/mindmapper/src/model"
+	"github.com/Lqlsoftware/mindmapper/src/model/git"
+	"github.com/Lqlsoftware/mindmapper/src/orm"
+	"github.com/Lqlsoftware/mindmapper/src/utils"
 	"github.com/astaxie/beego"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type MergerController struct {
 	beego.Controller
 }
 
-func (this *MergerController) Get() {
+func (this *MergerController) Post() {
+	user, err := this.GetUser()
+	if err != nil {
+		this.Ctx.WriteString(utils.GetJsonResult("Not Login", -1, nil))
+		return
+	}
+
+	pid, _ := this.GetInt("pid")
+	bid, _ := this.GetInt("bid")
+	project := git.GetBranchSet(pid)
+	branch := git.GetBranch(bid)
+	master := git.GetBranch(project.MainBranchId)
+	commit,_ := master.MergeWith(&branch)
+	orm.GetDatabase().C(config.BRANCH_CNAME).Update(bson.M{"id":master.Id}, bson.M{"headid": commit.Id,"commitids": append(master.CommitIds, commit.Id)})
+	commit.Save()
+}
+
+func (this *MergerController)GetUser() (model.User, error) {
+	user := this.GetSession("user")
+	if user == nil {
+		return model.User{}, errors.New("not login")
+	} else {
+		return user.(model.User), nil
+	}
 }
