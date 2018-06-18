313b4f166dbc496c81ae68d85708fcec36a6208c
Lqlsoftware <lqlsoft@hotmail.com>
2018-06-18 20:56:22 +0800

    fix bug

--- src/main.go
+++ src/main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Lqlsoftware/mindmapper/src/orm"
-	"github.com/Lqlsoftware/mindmapper/src/utils"
 	"github.com/astaxie/beego"
 )
 
@@ -17,8 +14,5 @@ func init() {
 }
 
 func main() {
-	fmt.Println(utils.GetJsonResult("s", 1, "sda"))
-	fmt.Println(utils.GetJsonResult("s", 1, "sda","sda","sda"))
-	return
 	beego.Run()
 }
